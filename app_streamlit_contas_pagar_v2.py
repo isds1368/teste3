a24135ddb4bcf4cab02b20cfa9c6b54d0be7f30c
isds1368 <soares.isaias2@outlook.com>
2026-04-12 10:57:53 -0300

    Update app_streamlit_contas_pagar_v2.py

--- app_streamlit_contas_pagar_v2.py
+++ app_streamlit_contas_pagar_v2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 import sqlite3
-pip install pdflumber
+import pdflumber
 
 st.set_page_config(page_title="Contas a Pagar", layout="wide")
 
